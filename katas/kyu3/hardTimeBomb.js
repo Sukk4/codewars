82dd0762b309d123ba46d7c72bad90e2990d5e30
Sukk4 <jukkaxkari@gmail.com>
2017-05-21 02:19:42 +0300

    Hard Time Bomb kata

---
+++ katas/kyu3/hardTimeBomb.js
@@ -0,0 +1,9 @@
+var wireCode; // Find the wire.
+console.log(Object.keys(this));
+for (var i=0; i<10; i++){
+  if (Object.keys(this).includes("boom"+i)){
+    console.log(this["boom"+i]);
+    wireCode = this["boom"+i];
+  }
+}
+Bomb.CutTheWire(wireCode);
